b9f392705f2bac12463b63e14cdb5e820e592790
amolc <amol.c@fountaintechies.com>
2022-04-14 23:47:16 +0800

    Added laptop details

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var serveStatic = require('serve-static');
+var bodyParser = require("body-parser");
+var multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+
+// handle cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", false);
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb", extended: true, type: "application/json" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, type: "application/x-www-form-urlencoding" }));
+app.use(bodyParser.json({ type: "application/json" }));
+app.use(bodyParser.raw({ limit: "50mb" }));
+
+
+app.use('/', express.static(__dirname + '/laptopshop'));
+app.use('/mangoshop', express.static(__dirname + '/mangoshop'));
+app.use('/mangoshop', express.static(__dirname + '/spurproject'));
+app.use('/mangoshop', express.static(__dirname + '/jobsite'));
+
+// app.get('/api/getvessellistById',vessels.getvessellistsById);  
+global.appRoot = path.resolve(__dirname);
+app.listen(9000);
+console.log('server is started at port: 9000');
