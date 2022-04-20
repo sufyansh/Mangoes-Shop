762831f4d897b90af5bf278418fe4a50c8143f69
amolc <amol.c@fountaintechies.com>
2022-04-20 19:47:59 +0800

    Added the org controller

--- admin/assets/js/orgCtrl.js
+++ admin/assets/js/orgCtrl.js
@@ -1,76 +1,75 @@
 app.controller(
-  "orgCtrl",
-  function ($scope, $http, $window, $location, $sce, $timeout, store) {
-    var baseurl = "https://api.superadmin.shop/api/";
+    "orgCtrl",
+    function($scope, $http, $window, $location, $sce, $timeout, store) {
+        var baseurl = "https://api.superadmin.shop/api/";
 
-    $scope.givealert = function (req, res) {
-      alert("I am alert");
-    };
+        $scope.givealert = function(req, res) {
+            alert("I am alert");
+        };
 
-    $scope.listorg = function (req, res) {
-      $http
-        .get(baseurl + "org/")
+        $scope.listorg = function(req, res) {
+            $http
+                .get(baseurl + "org/")
 
-        .success(function (res) {
-          if (res.status == "false") {
-          } else {
-            $scope.org = res.data;
-            console.log("org: ", $scope.org);
-          }
-        })
-        .error(function () {});
-    };
+            .success(function(res) {
+                    if (res.status == "false") {} else {
+                        $scope.organisations = res.data;
+                        console.log("org: ", $scope.organisations);
+                    }
+                })
+                .error(function() {});
+        };
 
-    $scope.redirect = function () {
-      //console.log("redirect");
-      location.href = "index.html";
-    };
+        $scope.redirect = function() {
+            //console.log("redirect");
+            location.href = "index.html";
+        };
 
-    //***************************************************************************************
-    //Save Mango
+        //***************************************************************************************
+        //Save Mango
 
-    $scope.orgadd = function () {
-      $scope.formvalidate = "true";
-      console.log("New Cars");
-      console.log($scope.data);
+        $scope.orgadd = function() {
+            $scope.formvalidate = "true";
+            console.log("New Cars");
+            console.log($scope.data);
 
-      $http
-        .post(baseurl + "org/", $scope.data)
-        .success(function (res) {
-          $scope.response = res;
-          console.log(res);
-          if (res.status == "false") {
-            alert(res.message);
-          } else {
-            alert("org save Successfully");
-            $window.location = "org.html";
-          }
-        })
-        .error(function () {
-          alert("Please check your internet connection or data source..");
-        });
-    };
+            $http
+                .post(baseurl + "org/", $scope.data)
+                .success(function(res) {
+                    $scope.response = res;
+                    console.log(res);
+                    if (res.status == "false") {
+                        alert(res.message);
+                    } else {
+                        alert("org save Successfully");
+                        $window.location = "org.html";
+                    }
+                })
+                .error(function() {
+                    alert("Please check your internet connection or data source..");
+                });
+        };
 
-    //orderCtrl ends
-    $scope.orgedit = function () {
-      $scope.formvalidate = "true";
-      console.log("New Cars");
-      console.log($scope.data);
+        //orderCtrl ends
+        $scope.orgedit = function() {
+            $scope.formvalidate = "true";
+            console.log("New Cars");
+            console.log($scope.data);
 
-      var Id = 1;
+            var Id = 1;
 
-      $http.patch(baseurl + "org/" + Id, $scope.data).success(function (res) {
-        $scope.response = res;
-        console.log(res);
-        if (res.status == "false") {
-          alert(res.message);
-        } else {
-          alert("org save Successfully");
-          $window.location = "org.html";
-        }
-        // }).error(function() {
-        //         // alert("Please check your internet connection or data source..");
-      });
-    };
-  }
+            $http.patch(baseurl + "org/" + Id, $scope.data).success(function(res) {
+                $scope.response = res;
+                console.log(res);
+                if (res.status == "false") {
+                    alert(res.message);
+                } else {
+                    alert("org save Successfully");
+                    $window.location = "org.html";
+                }
+                // }).error(function() {
+                //         // alert("Please check your internet connection or data source..");
+            });
+        };
+    }
 );
--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use('/admin', express.static(__dirname + '/admin'));
 app.use('/vueadmin', express.static(__dirname + '/vueadmin/public'));
 app.use('/fashion', express.static(__dirname + '/fashion'));
 app.use('/stones', express.static(__dirname + '/stones'));
+app.use('/stockmarketmantra', express.static(__dirname + '/stockmarketmantra'));
 
 // app.get('/api/getvessellistById',vessels.getvessellistsById);  
 global.appRoot = path.resolve(__dirname);
