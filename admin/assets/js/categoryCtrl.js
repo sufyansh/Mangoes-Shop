37b0b4292e6d18243577c3a55ad6c04fc36f0ce2
unknown <haris28398@gmail.com>
2022-04-21 17:38:33 +0500

    init

--- admin/assets/js/categoryCtrl.js
+++ admin/assets/js/categoryCtrl.js
@@ -1,58 +1,96 @@
-app.controller('categoryCtrl', function($scope, $http, $window, $location, $sce, $timeout, store) {
-
-
+app.controller(
+  "categoryCtrl",
+  function ($scope, $http, $window, $location, $sce, $timeout, store) {
     var baseurl = "https://api.superadmin.shop/api/";
 
+    $scope.givealert = function (req, res) {
+      alert("I am alert");
+    };
 
-    $scope.givealert = function(req, res) {
+    $scope.listcategories = function (req, res) {
+      $http
+        .get(baseurl + "category/")
 
-        alert("I am alert");
-    }
+        .success(function (res) {
+          if (res.status == "false") {
+          } else {
+            $scope.categories = res.data;
+            console.log("categories: ", $scope.categories);
+          }
+        })
+        .error(function () {});
+    };
 
-    $scope.listcategories = function(req, res) {
-
-        $http.get(baseurl + 'category/')
-
-        .success(function(res) {
-            if (res.status == 'false') {} else {
-                $scope.categories = res.data;
-                console.log('categories: ', $scope.categories);
-            }
-        }).error(function() {});
-    }
-
-
-    $scope.redirect = function() {
-        //console.log("redirect");
-        location.href = 'index.html';
-    }
-
+    $scope.redirect = function () {
+      //console.log("redirect");
+      location.href = "index.html";
+    };
 
     //***************************************************************************************
     //Save Mango
 
-    $scope.categoryadd = function() {
+    $scope.categoryadd = function () {
+      $scope.formvalidate = "true";
+      //console.log("New Cars");
+      console.log($scope.data);
 
-        $scope.formvalidate = "true";
-        //console.log("New Cars");
-        console.log($scope.data);
-
-
-
-        $http.post(baseurl + 'category/', $scope.data).success(function(res) {
-            $scope.response = res;
-            // console.log(res);
-            if (res.status == 'false') {
-                alert(res);
-            } else {
-                alert("Category save Successfully");
-                $window.location = "category.html";
-            }
-            // }).error(function() {
-            //         // alert("Please check your internet connection or data source..");
-        });
-    }
-
+      $http.post(baseurl + "category/", $scope.data).success(function (res) {
+        $scope.response = res;
+        // console.log(res);
+        if (res.status == "false") {
+          alert(res);
+        } else {
+          alert("Category save Successfully");
+          $window.location = "category.html";
+        }
+        // }).error(function() {
+        //         // alert("Please check your internet connection or data source..");
+      });
+    };
 
     //orderCtrl ends
-});
+
+    $scope.categoryedit = function (id) {
+      console.log(id);
+      $scope.formvalidate = "true";
+      console.log("New Cars");
+      console.log($scope.data);
+
+      var Id = 1;
+
+      $http.patch(baseurl + "category/" + id, $scope.data).success(function (res) {
+        $scope.response = res;
+        console.log(res);
+        if (res.status == "false") {
+          alert(res.message);
+        } else {
+          alert("category save Successfully");
+          $window.location = "category.html";
+        }
+        // }).error(function() {
+        //         // alert("Please check your internet connection or data source..");
+      });
+    };
+
+    $scope.categorydelete = function (id) {
+      console.log(id);
+      $scope.formvalidate = "true";
+      console.log("New delete");
+      console.log($scope.data);
+
+      var Id = 1;
+
+      $http.delete(baseurl + "category/" + id, $scope.data).success(function (res) {
+        $scope.response = res;
+        console.log(res);
+        if (res.status == "false") {
+          alert(res.message);
+        } else {
+          alert("category save Successfully deleted");
+          location.reload();
+          //   $window.location = "category.html";
+        }
+      });
+    };
+  }
+);
--- admin/assets/js/orgCtrl.js
+++ admin/assets/js/orgCtrl.js
@@ -1,77 +1,100 @@
 app.controller(
-    "orgCtrl",
-    function($scope, $http, $window, $location, $sce, $timeout, store) {
-        var baseurl = "https://api.superadmin.shop/api/";
+  "orgCtrl",
+  function ($scope, $http, $window, $location, $sce, $timeout, store) {
+    var baseurl = "https://api.superadmin.shop/api/";
 
-        $scope.givealert = function(req, res) {
-            alert("I am alert");
-        };
+    $scope.givealert = function (req, res) {
+      alert("I am alert");
+    };
 
-        $scope.listorg = function(req, res) {
-            $http
-                .get(baseurl + "org/")
+    $scope.listorg = function (req, res) {
+      $http
+        .get(baseurl + "org/")
 
-            .success(function(res) {
-                    if (res.status == "false") {} else {
-                        $scope.organisations = res.data;
-                        console.log("org: ", $scope.organisations);
-                    }
-                })
-                .error(function() {});
-        };
+        .success(function (res) {
+          if (res.status == "false") {
+          } else {
+            $scope.organisations = res.data;
+            console.log("org: ", $scope.organisations);
+          }
+        })
+        .error(function () {});
+    };
 
-        $scope.redirect = function() {
-            //console.log("redirect");
-            location.href = "index.html";
-        };
+    $scope.redirect = function () {
+      //console.log("redirect");
+      location.href = "index.html";
+    };
 
-        //***************************************************************************************
-        //Save Mango
+    //***************************************************************************************
+    //Save Mango
 
-        $scope.orgadd = function() {
-            $scope.formvalidate = "true";
-            console.log("New Cars");
-            console.log($scope.data);
+    $scope.orgadd = function () {
+      $scope.formvalidate = "true";
+      console.log("New Cars");
+      console.log($scope.data);
 
-            $http
-                .post(baseurl + "org/", $scope.data)
-                .success(function(res) {
-                    $scope.response = res;
-                    console.log(res);
-                    if (res.status == "false") {
-                        alert(res.message);
-                    } else {
-                        alert("org save Successfully");
-                        $window.location = "org.html";
-                    }
-                })
-                .error(function() {
-                    var msg = res;
-                    console.log(res)
-                    alert(res);
-                });
-        };
+      $http
+        .post(baseurl + "org/", $scope.data)
+        .success(function (res) {
+          $scope.response = res;
+          console.log(res);
+          if (res.status == "false") {
+            alert(res.message);
+          } else {
+            alert("org save Successfully");
+            $window.location = "org.html";
+          }
+        })
+        .error(function () {
+          var msg = res;
+          console.log(res);
+          alert(res);
+        });
+    };
 
-        //orderCtrl ends
-        $scope.orgedit = function() {
-            $scope.formvalidate = "true";
-            console.log("New Cars");
-            console.log($scope.data);
+    //orderCtrl ends
+    $scope.orgedit = function (id) {
+      console.log(id);
+      $scope.formvalidate = "true";
+      console.log("New Cars");
+      console.log($scope.data);
 
-            var Id = 1;
+      var Id = 1;
 
-            $http.patch(baseurl + "org/" + Id, $scope.data).success(function(res) {
-                $scope.response = res;
-                console.log(res);
-                if (res.status == "false") {
-                    alert(res.message);
-                } else {
-                    alert("org save Successfully");
-                    $window.location = "org.html";
-                }
-                // }).error(function() {
-                //         // alert("Please check your internet connection or data source..");
-            });
-        };
-    }
+      $http.patch(baseurl + "org/" + id, $scope.data).success(function (res) {
+        $scope.response = res;
+        console.log(res);
+        if (res.status == "false") {
+          alert(res.message);
+        } else {
+          alert("org save Successfully");
+          $window.location = "org.html";
+        }
+        // }).error(function() {
+        //         // alert("Please check your internet connection or data source..");
+      });
+    };
+
+    $scope.orgdelete = function (id) {
+      console.log(id);
+      $scope.formvalidate = "true";
+      console.log("New delete");
+      console.log($scope.data);
+
+      var Id = 1;
+
+      $http.delete(baseurl + "org/" + id, $scope.data).success(function (res) {
+        $scope.response = res;
+        console.log(res);
+        if (res.status == "false") {
+          alert(res.message);
+        } else {
+          alert("org save Successfully deleted");
+          location.reload();
+          //   $window.location = "org.html";
+        }
+      });
+    };
+  }
 );
